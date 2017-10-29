50ea213f27cefcc2442af507e2eca1b9e6d840db
mukilan29 <mukilan@batonsystems.com>
2017-10-29 21:26:05 +0530

    Login view created.

--- employee/views.py
+++ employee/views.py
@@ -1,6 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.contrib.auth import authenticate
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+
+class LoginView( APIView ):
+
+    def post(self, request, format=None):
+        user = authenticate( request = request )
+        if user is not None:
+            return Response( status = status.HTTP_200_OK )
+        else:
+            return Response( status = status.HTTP_404_NOT_FOUND )
+
+
