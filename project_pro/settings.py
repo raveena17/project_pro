89083dddd57d0740be8739a65787929fc47d17af
mukilan29 <mukilan@batonsystems.com>
2017-10-29 21:59:18 +0530

    apps were added in INSTALLED_APPS

--- project_pro/settings.py
+++ project_pro/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework', # rest framework
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'employee',
+    'project',
+    'task'
 ]
 
 MIDDLEWARE = [
