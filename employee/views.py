566cca4cff2f07190d3deb2be1879d7f4438c015
mukilan29 <mukilan@batonsystems.com>
2017-10-29 21:56:52 +0530

    Token rest package added for token authentication.

--- employee/views.py
+++ employee/views.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.contrib.auth import authenticate, login, logout
 
 from rest_framework.views import APIView
+from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -14,7 +15,10 @@ class LoginView( APIView ):
         user = authenticate( request=request )
         if user is not None:
             login(request, user)
-            return Response( status=status.HTTP_200_OK )
+            token = Token.objects.create( user=user )
+            return Response({
+                token: token
+            }, status=status.HTTP_200_OK )
         else:
             return Response( status=status.HTTP_404_NOT_FOUND )
 
