cbd9243231a9fde874b8d48c0d9935e7fd1b823e
mukilan29 <mukilan@batonsystems.com>
2017-10-31 13:23:07 +0530

    logout url added, and rest token authentication commented.

--- employee/views.py
+++ employee/views.py
@@ -12,13 +12,11 @@ from rest_framework import status
 class LoginView( APIView ):
 
     def post(self, request, format=None):
-        user = authenticate( request=request )
+        user = authenticate( **request.data )
         if user is not None:
             login(request, user)
-            token = Token.objects.create( user=user )
-            return Response({
-                token: token
-            }, status=status.HTTP_200_OK )
+            #token = Token.objects.create( user=user )
+            return Response(status=status.HTTP_200_OK )
         else:
             return Response( status=status.HTTP_404_NOT_FOUND )
 
