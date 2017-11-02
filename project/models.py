d97d4be1c00f5cd5dd2b41f28666402917e12c38
mukilan29 <mukilan@batonsystems.com>
2017-11-02 16:46:43 +0530

    table project-status, project, projectTeam, approval added.

--- project/models.py
+++ project/models.py
@@ -2,5 +2,45 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class ProjectStatus( models.Model ):
+    name = models.CharField(max_length=30)
+
+    class Meta:
+        db_table = 'ProjectStatus'
+
+
+class Approval( models.Model ):
+    approver = models.ForeignKey(User, on_delete=models.CASCADE)
+    approved_on = models.DateTimeField()
+    comment = models.TextField()
+    is_approved = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'Approval'
+
+
+class Project( models.Model ):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    status = models.ForeignKey(ProjectStatus, on_delete=models.CASCADE)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    approvers = models.ManyToManyField(Approval)
+    requested_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    requested_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'Project'
+
+
+class ProjectTeam( models.Model ):
+    name = models.CharField(max_length=30)
+    leads = models.ManyToManyField(User)
+    members = models.ManyToManyField(User)
+    project = models.ForeignKey(Project)
+
+    class Meta:
+        db_table = 'ProjectTeam'
