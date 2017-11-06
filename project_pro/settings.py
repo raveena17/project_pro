f69ad61c2e54a5a1c229c7ca5de31af378e59b12
mukilan29 <mukilan@batonsystems.com>
2017-11-06 13:53:59 +0530

    Fixtures added.

--- project_pro/settings.py
+++ project_pro/settings.py
@@ -123,3 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Fixtures Directory
+FIXTURE_DIRS = [
+    os.path.join(BASE_DIR, 'fixtures')
+]
